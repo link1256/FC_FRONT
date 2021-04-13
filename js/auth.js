20e37f911453cca37792d1fc43b4751f6b3639fe
ben771024 <ben771024@gmail.com>
2021-04-13 16:58:36 +0800

    登入頁面

---
+++ js/auth.js
@@ -0,0 +1,39 @@
+
+function captcha1() 
+{
+        let captcha1 = new CaptchaMini({
+            lineWidth: 1,   //線條寬度
+            lineNum: 6,       //線條數量
+            dotR: 2,          //點的半徑
+            dotNum: 25,       //點的數量
+            preGroundColor: [10, 80],    //前景色區間
+            backGroundColor: [150, 250], //背景色區間
+            fontSize: 60,           //字體大小
+            fontFamily: ['Microsoft JhengHei'],  //字體類型
+            fontStyle: 'fill',      //字體繪製方法，有fill和stroke
+            content: 'abcdefghijklmnopqrstuvwxyz',  //驗證碼内容
+            length: 4   //驗證碼
+        });
+        captcha1.draw(document.querySelector('#captcha1'), r => {
+            $(".captcha_a").val(r)
+        });
+}
+
+    $(".captcha1_icon").click(function () {
+        captcha1();
+    });
+
+    $("#loginbt_3").click(function () {
+        if ($(".captcha").val() === $(".captcha_a").val()) {
+            captcha1();
+            loginWithEmail().then(() => {
+                $('#login_email').trigger("reset");
+            })
+        }
+        else {
+            alert("驗證失敗");
+        }
+    });
+
+		  	  
+
