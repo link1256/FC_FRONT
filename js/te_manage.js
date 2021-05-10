4388fca40f15bfa12cb0ed0640abf86df0dab50e
ben771024 <ben771024@gmail.com>
2021-05-10 21:33:03 +0800

    事件異動設計

--- js/te_manage.js
+++ js/te_manage.js
@@ -6,3 +6,33 @@ function te_ini_page(i)
 	
 	
 }
+
+//子功能頁籤
+function step_tab()
+{
+	$(".te_manage_tab2 .nav-item3").on("click",function(){
+		 		  
+		  for(j = 1; j < 4; j++){
+			  if($(this).hasClass("tab"+j)){
+				  if(j==1){				
+					  create_step(2,j);						  
+				  }
+				  else if(j==2){
+	
+					  create_step(2,j);
+				  }
+				  else if(j==3){
+                      create_step(2,j);
+				  }				 
+			  }
+		  }		  
+		  $(this).parent().children().removeClass("active");
+		  $(this).addClass("active");
+	  });		
+}
+
+function create_step(i,j)
+{
+	$("#te_step").empty();
+	$("#te_step").load("./views/te_manage_tab"+i+"_step"+j+".html"); 
+}
