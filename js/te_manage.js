5260316fe09e99d9f53527fb42c16187b96d91e4
ben771024 <ben771024@gmail.com>
2021-05-20 18:41:32 +0800

    異動更新

--- js/te_manage.js
+++ js/te_manage.js
@@ -10,19 +10,41 @@ function te_ini_page(i)
 //子功能頁籤
 function step_tab()
 {
-	$(".te_manage_tab2 .nav-item3").on("click",function(){
+	$(".te_manage_tab2 .nav-item3:not(.active)").on("click",function(){
 		 		  
 		  for(j = 1; j < 4; j++){
 			  if($(this).hasClass("tab"+j)){
-				  if(j==1){				
-					  create_step(2,j);						  
+				  if(j==1){
+					  if($(this).parent().find(".active").hasClass("tab2")){
+						  return;
+					  }
+					  else{
+						  create_step(2,j);	
+					  }
+					  				  
 				  }
 				  else if(j==2){
-	
-					  create_step(2,j);
+					  if($(this).parent().find(".active").hasClass("tab2")){
+						  return;
+					  }
+					  else{
+						  if($(this).parent().find(".active").hasClass("tab3")){
+							create_step(2,j);  
+						  }
+						  else{
+							   if(confirm("是否建立新異動?")){
+			                create_step(2,j);
+						
+		                  }
+				          else{
+					           return;
+				              }
+						  }						 		
+					  }                 			  
 				  }
 				  else if(j==3){
                       create_step(2,j);
+					  
 				  }				 
 			  }
 		  }		  
@@ -33,6 +55,7 @@ function step_tab()
 
 function create_step(i,j)
 {
+
 	$("#te_step").empty();
 	$("#te_step").load("./views/te_manage_tab"+i+"_step"+j+".html"); 
 }
@@ -186,7 +209,7 @@ function loadDetialData(id){
 		  var item =
 		  "<div class='file_list_item'>"+
 	       "<a href=''><i class='fas fa-file-alt'></i>"+ "測試檔案" + i +"</a>"+
-		  "<button type='button' class='btn btn-danger file_list_item_delete'>刪除</button>"+
+		  "<button type='button' class='btn btn-danger file_list_item_delete '>刪除</button>"+
 	      "</div>"
 						
 		  $("#file_list").append(item);	
@@ -269,22 +292,76 @@ function loadDetialData(id){
 	}
 }
 
-function loadListData(id){
+function loadListData(fmtid){
 	/* 先塞測試用資料，需要改寫 */
-	//讀取資料
+	//讀取RELATION資料
 		
 		$(".list_frame").empty().load("./views/list_data_temp.html"); //模板
 		
-		setTimeout(function(){
-		//序號
-		$("#te_data_basic_state").addClass("state_type1").text("未送審");
-	    //類型
-		$("#te_data_basic_uid").text("1100510001");
-		//圖資識別碼
-		$("#te_data_basic_createuser").text("測試者1");
-		//管理
-		$("#te_data_basic_createtime").text("2021/05/10 22:00");
+		let type1 = "國有林事業區";
+		let type2 = "保安林";
 		
+		let edit_type1 = "新增";
+		let edit_type2 = "編修";
+		let edit_type3 = "刪除";
+
+		setTimeout(function(){
+			
+		var rows;      
+		var FM_ID;
+		
+		for(i=1;i < 4;i++){
+			FM_ID = 'FM'+i;
+		if(i==3){
+			rows = "<tr id='"+ FM_ID +"'><td>"+ i +"</td>" +
+	           "<td class='typeid'>"+ type1 +"</td>" +
+		       "<td>"+ edit_type3 +"</td>"+
+	           "<td><button type='button' class='btn btn-danger search_submit custom_bt3 manage_bt_area' onclick='doRemove("+ FM_ID +")'>移除</button></td></tr>";
+             $("#list_table").append(rows);	
+		}
+		else{
+			rows = "<tr id='"+ FM_ID +"'><td>"+ i +"</td>" +
+	            "<td class='typeid'>"+ type1 +"</td>" +
+		       "<td>"+ edit_type2 +"</td>"+
+	           "<td><button type='button' class='btn btn-warning search_submit custom_bt3 manage_bt_area' onclick='doEdit("+ FM_ID +")'>編輯</button></td></tr>";
+           $("#list_table").append(rows);	
+		}
+		
+		}
+		
+		for(i=4;i < 7;i++){
+			FM_ID = 'FM'+i;
+		if(i==6){
+			rows = "<tr id='"+ FM_ID +"'><td>"+ i +"</td>" +
+	           "<td class='typeid'>"+ type2 +"</td>" +
+		       "<td>"+ edit_type3 +"</td>"+
+	           "<td><button type='button' class='btn btn-danger search_submit custom_bt3 manage_bt_area' onclick='doRemove("+ FM_ID +")'>移除</button></td></tr>";
+             $("#list_table").append(rows);	
+		}
+		
+		else{
+			rows = "<tr id='"+ FM_ID +"'><td>"+ i +"</td>" +
+	            "<td class='typeid'>"+ type2 +"</td>" +
+		       "<td>"+ edit_type2 +"</td>"+
+	           "<td><button type='button' class='btn btn-warning search_submit custom_bt3 manage_bt_area' onclick='doEdit("+ FM_ID +")'>編輯</button></td></tr>";
+           $("#list_table").append(rows);	
+		}
+		}
+		
+		
+		$("#list_table tr").on("click",function(){
+		$(this).parent().children().removeClass("active");
+		$(this).addClass("active");
+	 	
+        });
+		
+		//圖資列表點擊事件
+		$("#list_table tr").on("click",function(){
+			let fmid = $(this).attr('id');
+			let typeid = $(this).find(".typeid").text();
+			FM_detail(typeid,fmid);
+		});
+			
 		},50);
 				
 }
@@ -296,4 +373,41 @@ function newApplyDetail(){
 		$("#te_data_basic_new_createtime").text("2021/05/13 11:00");
 		//所屬單位	
 		$("#te_data_basic_new_deptart").text("新竹林區管理處");
+}
+
+function FM_detail(typeid,fmid)//圖資屬性資料
+{
+	$("#list_detail").empty();
+	//資料分成國有林事業區及保安林
+	if(typeid === "國有林事業區"){
+		let detail;
+		detail = "<div></div>";
+		
+		
+		
+		
+		
+		
+		$("#list_detail").append("<div>test1"+ fmid +"</div>");
+		
+	}
+	else{
+		$("#list_detail").append("<div>test2"+ fmid +"</div>");
+	}
+	
+}
+
+function doCreate()//新增圖資
+{
+	
+}
+
+function doEdit(fmid)//編輯圖資
+{
+	
+}
+
+function doRemove(fmid)//移除待異動圖資
+{
+	confirm("確定要移除本項異動?");
 }
